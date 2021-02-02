a66bb218d7684a51d87390806eaaeb2b3d2ffca7
pradeeps9 <46450532+pradeeps9@users.noreply.github.com>
2021-02-02 22:46:46 +0530

    Update hmsys.py

--- HealthManagementSystem/hmsys.py
+++ HealthManagementSystem/hmsys.py
@@ -8,11 +8,8 @@ act = input("What do you want to do Lock/Retrieve data?\n 1 to Lock &\n 2 to Ret
 client = input("Choose whose client:\n 1 for Pradeep\n 2 for Sumeet\n 3 for Kishan")
 scdl = input("Choose from:\n 1 for Diet\n 2 for Exercise")
 
-# print(act + client + scdl)...
-
-
 def hms(filename, act1):
-    """This function retrieve and collect data depends on the value of act"""
+    """This function retrieve or lock data depends on the value of act"""
     if act1 == "1":
         with open(filename, "a") as etf:
             cmnt = "[" + str(getdate()) + "]" + input("Write here") + "\n"
@@ -22,16 +19,16 @@ def hms(filename, act1):
         with open(filename, "r") as otf:
             print(otf.read())
 
-
 if client == "1" and scdl == "1":
-    hms("Pradeep_Diet", act)
+    hms("Harry_Diet", act)
 elif client == "1" and scdl == "2":
-    hms("Pradeep_Ex", act)
+    hms("Harry_Ex", act)
 elif client == "2" and scdl == "1":
-    hms("Sumeet_Diet", act)
+    hms("Rohan_Diet", act)
 elif client == "2" and scdl == "2":
-    hms("Sumeet_Ex", act)
+    hms("Rohan_Ex", act)
 elif client == "3" and scdl == "1":
-    hms("Kishan_Diet", act)
+    hms("Hammad_Diet", act)
 elif client == "3" and scdl == "2":
-    hms("Kishan_Ex", act)
+    hms("Hammad_Ex", act)
+
